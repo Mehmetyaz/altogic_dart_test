269c817e117a5cbb684f80a948bd317ce3d72152
mehmet <mehmedyaz@gmail.com>
2022-06-13 14:05:28 +0300

    ✅ bucket_manager - make_public - success test passed.

--- test/bucket_manager_test.dart
+++ test/bucket_manager_test.dart
@@ -130,4 +130,27 @@ void main() {
 
     expect(result, isNotNull);
   });
+
+  test('make_public', () async {
+    await removeTestBucket();
+    await createBucketTest(isPublic: false);
+
+    var existsRes = await bucketExists();
+
+    expect(existsRes.errors, isNull);
+    expect(existsRes.data, isNotNull);
+
+    var result = await testBucket().makePublic();
+
+    print(result.data);
+
+    expect(result.data, isNotNull);
+    expect(result.errors, isNull);
+
+    var info = await getBucketInfo();
+
+    expect(info.errors, isNull);
+    expect(info.data, isNotNull);
+    expect(info.data!['isPublic'], true);
+  });
 }
