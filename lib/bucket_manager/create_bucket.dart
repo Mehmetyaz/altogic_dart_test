18c26fa981b466da22d3b064ddd50dc52cac7d50
mehmet <mehmedyaz@gmail.com>
2022-06-13 13:29:49 +0300

    ✅ bucket_manager - get_info - success test passed.

--- lib/bucket_manager/create_bucket.dart
+++ lib/bucket_manager/create_bucket.dart
@@ -7,10 +7,16 @@ Future<APIError?> removeTestBucket() async {
   return client.storage.bucket(testBucketName).delete();
 }
 
-Future<APIResponse<Map<String, dynamic>>> createBucketTest() async {
-  return client.storage.createBucket(testBucketName);
+Future<APIResponse<Map<String, dynamic>>> createBucketTest(
+    {bool isPublic = false, List<String> tags = const []}) async {
+  return client.storage
+      .createBucket(testBucketName, tags: tags, isPublic: isPublic);
 }
 
 Future<APIResponse<bool>> bucketExists() async {
   return client.storage.bucket(testBucketName).exists();
+}
+
+Future<APIResponse<Map<String, dynamic>>> getBucketInfo() async {
+  return client.storage.bucket(testBucketName).getInfo();
 }
--- test/bucket_manager_test.dart
+++ test/bucket_manager_test.dart
@@ -5,27 +5,55 @@ import 'package:test/expect.dart';
 import 'package:test/scaffolding.dart';
 
 void main() {
-  group('exists', () {
-    test('success', () async {
-      createClientTest();
+  test(skip: true, 'exists', () async {
+    createClientTest();
 
-      await setUpEmailUser();
+    await setUpEmailUser();
 
-      await removeTestBucket();
-      var exists = await bucketExists();
+    await removeTestBucket();
+    var exists = await bucketExists();
 
-      expect(exists.errors, isNull);
-      expect(exists.data, false);
+    expect(exists.errors, isNull);
+    expect(exists.data, false);
 
-      var createResult = await createBucketTest();
+    var createResult = await createBucketTest();
 
-      expect(createResult.errors, isNull);
-      expect(createResult.data, isNotNull);
+    expect(createResult.errors, isNull);
+    expect(createResult.data, isNotNull);
 
-      exists = await bucketExists();
+    exists = await bucketExists();
 
-      expect(exists.errors, isNull);
-      expect(exists.data, true);
-    });
+    expect(exists.errors, isNull);
+    expect(exists.data, true);
+  });
+
+  test('get_info', () async {
+    createClientTest();
+
+    await setUpEmailUser();
+
+    await removeTestBucket();
+    var bucketInfo = await getBucketInfo();
+
+    expect(bucketInfo.errors, isNotNull);
+    expect(bucketInfo.data, isNull);
+
+    var createResult =
+        await createBucketTest(isPublic: true, tags: ['test_tag1']);
+
+    expect(createResult.errors, isNull);
+    expect(createResult.data, isNotNull);
+
+    bucketInfo = await getBucketInfo();
+
+
+    expect(bucketInfo.errors, isNull);
+    expect(bucketInfo.data, isNotNull);
+
+    var data = bucketInfo.data!;
+
+    expect(data['name'], testBucketName);
+    expect(data['isPublic'], true);
+    expect(data['tags'], contains('test_tag1'));
   });
 }
