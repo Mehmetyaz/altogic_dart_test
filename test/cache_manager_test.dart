86eae3627fb04700dc53d3c2bb08f35676921a3d
mehmet <mehmedyaz@gmail.com>
2022-06-19 12:34:54 +0300

    ✅ cache_manager - increment/decrement - 4 test passed.

--- test/cache_manager_test.dart
+++ test/cache_manager_test.dart
@@ -100,5 +100,59 @@ void main() {
       expect(value.data, isNull);
       expect(value.errors, isNull);
     });
+
+    group('increment/decrement', () {
+      test('success', () async {
+        var result = await addTestCache(10);
+
+        expect(result, successResponse);
+
+        var increment = await client.cache.increment(testCacheKey, 1);
+
+        expect(increment, successResponse);
+
+        var cache = await getTestCache().asInt();
+
+        expect(cache, successResponse);
+
+        expect(cache.data, 11);
+
+        var decrement = await client.cache.decrement(testCacheKey, 5);
+
+        expect(decrement, successResponse);
+
+        cache = await getTestCache().asInt();
+
+        expect(cache, successResponse);
+
+        expect(cache.data, 6);
+      });
+
+      test('not_exists_inc', () async {
+        await client.cache.delete('not_exists_inc');
+        var increment = await client.cache.increment('not_exists_inc', 1);
+
+        expect(increment, successResponse);
+
+        var cache = await client.cache.get('not_exists_inc').asInt();
+
+        expect(cache, successResponse);
+        expect(cache.data, 1);
+      });
+
+
+      test('not_exists_dec', () async {
+        await client.cache.delete('not_exists_dec');
+        var increment = await client.cache.decrement('not_exists_dec', 1);
+
+        expect(increment, successResponse);
+
+        var cache = await client.cache.get('not_exists_dec').asInt();
+
+        expect(cache, successResponse);
+        expect(cache.data, -1);
+      });
+
+    });
   });
 }
